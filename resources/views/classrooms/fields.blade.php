852da2ec099fe3cc49138f78d5f02b50153e2db5
Fabio <fpviviani@gmail.com>
2021-07-27 12:50:15 -0300

    Clasrooms fields correction

--- resources/views/classrooms/fields.blade.php
+++ resources/views/classrooms/fields.blade.php
@@ -14,7 +14,7 @@
 <div class="form-group col-sm-6">
     <div class="form-check">
         {!! Form::hidden('availability', 0, ['class' => 'form-check-input']) !!}
-        {!! Form::checkbox('availability', '1', null, ['class' => 'form-check-input']) !!}
+        {!! Form::checkbox('availability', '1', null, ['class' => 'form-check-input', 'id' => 'classroom-checkbox']) !!}
         {!! Form::label('availability', 'Está Disponível?', ['class' => 'form-check-label']) !!}
     </div>
 </div>  
